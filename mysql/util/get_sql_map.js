d5ae699da5b27689b5e4b41cc1f1ae1deaf9e43b
fangjianhui <fangjianhui77@163.com>
2019-10-18 17:25:58 +0800

    mysql建表

---
+++ mysql/util/get_sql_map.js
@@ -0,0 +1,21 @@
+// get_sql_map.js 获取所有sql脚本文件
+const fs = require('fs')
+const walkFile = require('./walk_file')
+
+/**
+ * 获取sql目录下的文件数据
+ * @return {object}
+ */
+function getSqlMap () {
+  let basePath = __dirname
+  let pathArr
+  let fileList
+
+  basePath = basePath.replace(/\\/g, '\/')
+  pathArr = basePath.split('\/').splice(0, pathArr.length - 1)
+  basePath = pathArr.join('/') + '/sql/'
+  fileList = walkFile(basePath, 'sql')
+  return fileList
+}
+
+module.exports = getSqlMap
